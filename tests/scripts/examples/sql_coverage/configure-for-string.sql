83e815213e274f53becdfbbd5f9874acad9efd88
jcrawford <jcrawford@voltdb.com>
2014-10-20 17:49:02 -0400

    configure-for-string.sql: add (to both partitioned & replicated table) VarChar columns, with lengths: 16 (characters), 40 bytes, 63 bytes, 64 bytes; give all of these, plus the existing columns a 30% chance of being null; and any of them many be used on either side of an UPDATE statement.

--- tests/scripts/examples/sql_coverage/configure-for-string.sql
+++ tests/scripts/examples/sql_coverage/configure-for-string.sql
@@ -14,12 +14,12 @@
 {@idcol = "ID"}
 
 
-{@insertvals = "_id, _value[string], _value[string null30], _value[float]"}
+{@insertvals = "_id, _value[string null30], _value[string null30], _value[string null30], _value[string null30], _value[string null30], _value[string null30], _value[float]"}
 -- There are no unary string-to-string functions supported yet.
 {@onefun = ""}
 --{@onefun = "LOWER"}
 {@optionalfn = "_stringfun"}
 
-{@updatecolumn = "DESC"}
-{@updatesource = "DESC_INLINE"}
+{@updatecolumn = "_variable[string]"}
+{@updatesource = "_variable[string]"}
 {@updatevalue = "_value[string]"}
