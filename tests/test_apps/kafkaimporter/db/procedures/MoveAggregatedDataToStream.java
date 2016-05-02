a07589915b5d45c7114faa5e3983adc476dfdffe
Peter Shaw <pshaw@voltdb.com>
2016-05-02 13:27:34 -0400

    ENG-10323 Pass licensecheck + working

--- tests/test_apps/kafkaimporter/db/procedures/MoveAggregatedDataToStream.java
+++ tests/test_apps/kafkaimporter/db/procedures/MoveAggregatedDataToStream.java
@@ -39,7 +39,9 @@
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
  * License for the specific language governing permissions and limitations under
  * the License.
- */package db.procedures;
+ */
+
+package db.procedures;
 
 import org.voltdb.SQLStmt;
 import org.voltdb.VoltProcedure;
