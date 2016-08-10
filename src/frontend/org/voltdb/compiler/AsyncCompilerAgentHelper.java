e2403a5ab79a17c61de0f4ba2e2f2ee8614fab74
Paul Martel <pmartel@voltdb.com>
2016-08-10 17:25:28 -0400

    Merged parameter conversion diagnostics from 6.2 special, commits b8221047 and 9e362416.

--- src/frontend/org/voltdb/compiler/AsyncCompilerAgentHelper.java
+++ src/frontend/org/voltdb/compiler/AsyncCompilerAgentHelper.java
@@ -125,8 +125,8 @@ public class AsyncCompilerAgentHelper
                     return retval;
                 }
                 catch (Throwable t) {
-                    retval.errorMsg = "Unexpected condition occurred applying DDL statements: " +
-                        t.toString();
+                    retval.errorMsg = "Unexpected condition applying DDL statements to " +
+                        "original catalog: " + t.toString();
                     compilerLog.error(retval.errorMsg);
                     return retval;
                 }
@@ -236,7 +236,7 @@ public class AsyncCompilerAgentHelper
         catch (Exception e) {
             String msg = "Unexpected error in adhoc or catalog update: " + e.getClass() + ", " +
                 e.getMessage();
-            compilerLog.warn(msg, e);
+            compilerLog.error(msg, e);
             retval.encodedDiffCommands = null;
             retval.errorMsg = msg;
         }
