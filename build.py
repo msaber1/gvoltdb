d4ad83f620ec4048d709a1f4d81b18060de9667a
alexeevm <michael.alexeev@gmail.com>
2016-09-05 18:31:39 -0400

    MP set op - refactoring plus renaming pluse unit tests

--- build.py
+++ build.py
@@ -281,9 +281,9 @@ CTX.INPUT['executors'] = """
  receiveexecutor.cpp
  sendexecutor.cpp
  seqscanexecutor.cpp
+ setopexecutor.cpp
  tablecountexecutor.cpp
  tuplescanexecutor.cpp
- unionexecutor.cpp
  updateexecutor.cpp
 """
 
@@ -327,8 +327,8 @@ CTX.INPUT['plannodes'] = """
  SchemaColumn.cpp
  sendnode.cpp
  seqscannode.cpp
+ setopnode.cpp
  tuplescannode.cpp
- unionnode.cpp
  updatenode.cpp
 """
 
